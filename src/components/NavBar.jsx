04a747fde052555ffc93d9d758fa932f90054033
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-09-11 18:38:20 -0300

    creación del componente navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./NavBar.css";
+
+export default function NavBar({ brandImg }) {
+	return (
+		<nav>
+			<div>
+				<a href="#">
+					<img src={brandImg} alt="logo" className="nav--logo" />
+				</a>
+			</div>
+			<ul className="nav--items">
+				<li className="nav-item">
+					<a href="#">Home</a>
+				</li>
+				<li className="nav-item">
+					<a href="#">Products</a>
+				</li>
+				<li className="nav-item">
+					<a href="#">About Us</a>
+				</li>
+				<li className="nav-item">
+					<a href="#">Cart</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
