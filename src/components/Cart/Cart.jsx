6921fc1e01c9dfec171370ee3badbca71f2a83cb
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-10-06 10:05:48 -0300

    creacion itemColumnlist + carrito vacio mensaje+ agregado de estilos landscapecard I

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,21 +1,37 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceSadCry } from "@fortawesome/free-solid-svg-icons";
 
 import { cartContext } from "../../contexts/cartContext";
-import LandscapeCard from "../LandscapeCard/LandscapeCard";
+import ItemColumnList from "../ItemColumnList/ItemColumnList";
 
 const Cart = () => {
-	const { cart, getTotalPrice } = useContext(cartContext);
+	const { cart, getTotalPrice, getCartCount } = useContext(cartContext);
 
 	return (
 		<main>
 			<section className="list--box">
 				<h1>Carrito</h1>
-				<ul>
-					{cart.map((item) => (
-						<LandscapeCard key={item.id} item={item} mode="cart" />
-					))}
-				</ul>
-				<h3>El total es {getTotalPrice()}</h3>
+				{!getCartCount() > 0 ? (
+					<React.Fragment>
+						<h2>
+							El carrito se encuentra vacío{" "}
+							<span>
+								<FontAwesomeIcon icon={faFaceSadCry} />
+							</span>
+						</h2>
+						<Link to="/" style={{ color: "grey" }}>
+							¡Te invitamos a ver nuestros productos!
+						</Link>
+					</React.Fragment>
+				) : (
+					<React.Fragment>
+						<ItemColumnList items={cart} />
+						<h3>El total es {getTotalPrice()}</h3>
+					</React.Fragment>
+				)}
 			</section>
 		</main>
 	);
---
+++ src/components/ItemColumnList/ItemColumnList.jsx
@@ -0,0 +1,13 @@
+import LandscapeCard from "../LandscapeCard/LandscapeCard";
+
+const ItemColumnList = ({ items }) => {
+	return (
+		<section style={{ display: "flex", flexDirection: "column" }}>
+			{items.map((item) => (
+				<LandscapeCard key={item.id} item={item} mode="cart" />
+			))}
+		</section>
+	);
+};
+
+export default ItemColumnList;
