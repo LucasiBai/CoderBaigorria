6b5d7e5bb9b3b459e40ac4e0fbf4e24e780e3ee1
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-10-23 19:56:24 -0300

    creación de custom hook useCarousel + agregado de autoplay al carrusel

--- src/components/FullCarousel/FullCarousel.jsx
+++ src/components/FullCarousel/FullCarousel.jsx
@@ -1,73 +1,32 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
+import useCarousel from "../../hooks/useCarousel";
 
 import FullCarouselImg from "../FullCarouselImg/FullCarouselImg";
 import MoveButton from "../MoveButton/MoveButton";
 
 import "./FullCarousel.css";
 const FullCarousel = ({ images, autoPlay }) => {
-	const [currentIdx, setCurrentIdx] = useState({
-		previous: images.length - 1,
-		center: 0,
-		next: 1,
-	});
-
-	const [currentImgs, setCurrentImgs] = useState([
-		images[currentIdx.previous],
-		images[currentIdx.center],
-		images[currentIdx.next],
-	]);
+	const [autoMove, setAutoMove] = useState(autoPlay);
 
 	const imgsContainer = useRef(null);
-
-	const changeCurrentImgs = (direction) => {
-		const nextImgs = { ...currentIdx };
-		if (direction === "left") {
-			const setLeft = (key) => {
-				nextImgs[key] =
-					nextImgs[key] > 0 ? nextImgs[key] - 1 : images.length - 1;
-			};
-			setLeft("previous");
-			setLeft("center");
-			setLeft("next");
-		} else {
-			const setRight = (key) => {
-				nextImgs[key] =
-					nextImgs[key] !== images.length - 1 ? nextImgs[key] + 1 : 0;
-			};
-			setRight("previous");
-			setRight("center");
-			setRight("next");
-		}
-
-		setCurrentIdx(nextImgs);
-		setCurrentImgs([
-			images[nextImgs.previous],
-			images[nextImgs.center],
-			images[nextImgs.next],
-		]);
-	};
-
-	const changeImg = (direction) => {
-		imgsContainer.current.style.transition = "350ms ease transform";
-		imgsContainer.current.style.transform = `translateX(${
-			direction === "left" ? "" : "-"
-		}1150px)`;
-
-		const endTrigger = () => {
-			changeCurrentImgs(direction);
-			imgsContainer.current.removeEventListener("transitionend", endTrigger);
-		};
-
-		imgsContainer.current.addEventListener("transitionend", endTrigger);
-	};
+	const [currentImgs, changeImg] = useCarousel(images, imgsContainer);
 
 	useEffect(() => {
-		imgsContainer.current.style.transition = "none";
-		imgsContainer.current.style.transform = "translateX(0)";
-	}, [currentImgs, currentIdx]);
+		if (autoMove) {
+			const interval = setInterval(() => {
+				changeImg();
+			}, 5000);
+			return () => clearInterval(interval);
+		}
+	});
 
 	return (
-		<section className="full-carousel__box">
+		<section
+			className="full-carousel__box"
+			onMouseEnter={() => autoPlay && setAutoMove(false)}
+			onMouseLeave={() => autoPlay && setAutoMove(true)}
+		>
 			<div className="full-carousel">
 				<MoveButton
 					direction="left"
@@ -77,7 +36,7 @@ const FullCarousel = ({ images, autoPlay }) => {
 				<MoveButton
 					direction="right"
 					className={"carousel-right"}
-					handleSlide={() => changeImg("right")}
+					handleSlide={changeImg}
 				/>
 				<FullCarouselImg images={currentImgs} reference={imgsContainer} />
 			</div>
---
+++ src/hooks/useCarousel.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+
+const useCarousel = (images, ref) => {
+	const [currentIdx, setCurrentIdx] = useState({
+		previous: images.length - 1,
+		center: 0,
+		next: 1,
+	});
+
+	const [currentImgs, setCurrentImgs] = useState([
+		images[currentIdx.previous],
+		images[currentIdx.center],
+		images[currentIdx.next],
+	]);
+
+	const imgsContainer = ref;
+
+	const changeCurrentImgs = (direction) => {
+		const nextImgs = { ...currentIdx };
+		if (direction === "left") {
+			const setLeft = (key) => {
+				nextImgs[key] =
+					nextImgs[key] > 0 ? nextImgs[key] - 1 : images.length - 1;
+			};
+			setLeft("previous");
+			setLeft("center");
+			setLeft("next");
+		} else {
+			const setRight = (key) => {
+				nextImgs[key] =
+					nextImgs[key] !== images.length - 1 ? nextImgs[key] + 1 : 0;
+			};
+			setRight("previous");
+			setRight("center");
+			setRight("next");
+		}
+
+		setCurrentIdx(nextImgs);
+		setCurrentImgs([
+			images[nextImgs.previous],
+			images[nextImgs.center],
+			images[nextImgs.next],
+		]);
+	};
+
+	const changeImg = (direction) => {
+		imgsContainer.current.style.transition = "350ms ease transform";
+		imgsContainer.current.style.transform = `translateX(${
+			direction === "left" ? "" : "-"
+		}1400px)`;
+
+		const endTrigger = () => {
+			changeCurrentImgs(direction);
+			imgsContainer.current.removeEventListener("transitionend", endTrigger);
+		};
+
+		imgsContainer.current.addEventListener("transitionend", endTrigger);
+	};
+
+	useEffect(() => {
+		imgsContainer.current.style.transition = "none";
+		imgsContainer.current.style.transform = "translateX(0)";
+	}, [currentImgs, currentIdx, imgsContainer]);
+
+	return [currentImgs, changeImg];
+};
+
+export default useCarousel;
